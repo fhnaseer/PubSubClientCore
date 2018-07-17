911ee2832e5a82b8c320e6b6752d3b2e904f9a42
fhnaseer <fhnaseer@live.com>
2018-07-17 02:07:03 +0200

    Adding base project,

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PubSubClientCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
